2c300c345e6d21ff174bfbb3877005d45033db33
Alejandro Pérez Martín <aleperez92@gmail.com>
2015-11-24 22:57:23 +0000

    Added mongoose package

--- app.js
+++ app.js
@@ -6,27 +6,6 @@ var cookieParser = require('cookie-parser');
 var path = require('path');
 var jade = require('jade');
 var routes = require('./routes');
-var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
 var app = express();
-
-MongoClient.connect('mongodb://localhost:27017/blog', function (err, db) {
-
-    if (err) throw err;
-
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: false
-    }));
-    app.use(cookieParser());
-
-    routes(app, db);
-
-    app.listen(3000, function () {
-        console.log('Server running on: localhost:3000')
-    });
-
-});
